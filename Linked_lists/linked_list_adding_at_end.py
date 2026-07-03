0c95d8483afe0bffbc435e5a7aed3e2e75ec4f72
Sai_kumarG <grkvp.sai@gmail.com>
2026-07-03 08:31:55 +0530

    Linked lists

---
+++ Linked_lists/linked_list_adding_at_end.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+class linked_list():
+    def __init__(self):
+        self.head=None
+    def show(self):
+        if self.head is None:
+            return None
+        temp=self.head
+        while temp:
+            print(temp.data,"-->",end="")
+            temp=temp.next
+        print("None")
+    def add(self,data):
+        new_node=Node(data)
+        if self.head is None:
+            self.head=new_node
+            return 
+        temp=self.head
+        while temp.next:
+            temp=temp.next
+        temp.next=new_node
+l=linked_list()
+li=list(map(int,input().split(",")))
+for i in li:
+    l.add(i)
+l.show()

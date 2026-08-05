67172afb244ffdda227dca5973c97a70cfbcc70c
Sai_KumarG <grkvp.sai@gmail.com>
2026-08-05 14:25:53 +0530

    data types

---
+++ data_types/sets.py
@@ -0,0 +1,14 @@
+#set is a collection of unique unodered elements
+#set=no duplicates
+#set =can be in any order(random order)
+li=[1,2,3,4,5]
+set1=set(li)
+print(set1)
+print(type(set1))
+st="aaabbccd"
+set2=set(st)
+print(type(set2))
+set3={1,1,2,2,3,3,3,3,4}
+print(set3)
+set4={'a','b','c','d','d','e','e','f'}
+print(set4)
